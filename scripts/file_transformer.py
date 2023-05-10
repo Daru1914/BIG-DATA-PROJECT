fc4c404776e2fd5981b62da83f25d840bc58d999
Daru1914 <daru1914@gmail.com>
2023-05-10 16:34:00 +0300

    reworked csv

---
+++ scripts/file_transformer.py
@@ -0,0 +1,29 @@
+"""
+This module cleans up after messy write to csv from the cmd
+"""
+import os
+import sys
+
+# specify input and output file paths
+INPUT = sys.argv[1]
+
+# read input file and write output file
+with open('output/out.csv', 'w') as f_out:
+    with open(INPUT, 'r') as f_in:
+        # skip header
+        header = next(f_in)
+        for line in f_in:
+            # split line by comma
+            line_parts = line.strip().split(',')
+            # join all items except last as a single string
+            NEW_LINE = ','.join(line_parts[:-1])
+            # add the last item to the new line
+            NEW_LINE += ',' + line_parts[-1]
+            # write new line to output file
+            f_out.write(NEW_LINE + '\n')
+
+# remove old file
+os.remove(INPUT)
+
+# rename output file as old file name
+os.rename('output/out.csv', INPUT)
